92507b000a0f235878e2344cc1e6ae3d714d3ca6
keerthana <keerthana.m@iamneo.ai>
2024-10-24 19:53:31 +0530

    htacess

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'https://backend-812956739285.us-east4.run.app/api',  // Development API URL
+    // apiUrl: 'http://localhost:3000/api',  // Development API URL
   };
   

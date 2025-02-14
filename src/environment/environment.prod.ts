5a618850e17df2b9f99accc16711dffcd51d32e5
keerthana <keerthana.m@iamneo.ai>
2025-02-14 11:57:50 +0530

    iphone

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     // apiUrl: 'https://rashtrotthana-backend-812956739285.us-east4.run.app/api'  // Production API URL
   //  apiUrl: 'http://localhost:3000/api' 
-    apiUrl: 'https://backend-812956739285.us-east4.run.app/api',
+    apiUrl: 'https://backend-812956739285.us-east4.run.app/api', 
   };

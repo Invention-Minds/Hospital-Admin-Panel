49e253e3c7ac4dd0f525fd54b90d6fe0bc77754a
keerthana <keerthana.m@iamneo.ai>
2025-02-18 22:35:07 +0530

    phase 2

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://rashtrotthana-backend-812956739285.us-east4.run.app/api'  // Production API URL
-   apiUrl: 'http://localhost:3000/api' 
+    apiUrl: 'https://rashtrotthana-backend-812956739285.us-east4.run.app/api'  // Production API URL
+  //  apiUrl: 'http://localhost:3000/api' 
     // apiUrl: 'https://backend-812956739285.us-east4.run.app/api',
   };

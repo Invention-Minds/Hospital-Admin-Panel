0d25de4d8409197b8efa78c7229aa3a8ee1ae524
keerthana <keerthana.m@iamneo.ai>
2025-11-22 17:10:10 +0530

    env-fike

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: true,
     // apiUrl: 'https://rashtrotthana-backend-812956739285.us-east4.run.app/api'  // Production API URL
-    //    apiUrl: 'http://192.168.9.139:3000/api', 
+       apiUrl: 'http://192.168.9.139:3000/api', 
     //    apiUrl:'http://34.93.41.27:3000/api'
     // apiUrl: 'https://backend-812956739285.us-east4.run.app/api',
     // apiUrl:'https://hrproindia.in/api'
     // apiUrl:'http://backendapi.hrproindia.in/api'
     // apiUrl:'http://ec2-13-60-191-28.eu-north-1.compute.amazonaws.com:3000/api',
-    apiUrl: 'http://localhost:3000/api',
+    // apiUrl: 'http://localhost:3000/api',
     // apiUrl:"https://vasavi-hospitals-812956739285.us-east4.run.app/api"
 
 };

95f2b3e72b8def77cf34ec4b982fc4d43feea0ba
Keerthana M <keerthanasaminathan0805@gmail.com>
2026-04-11 17:23:27 +0530

    .env

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,18 +1,21 @@
 const isLan = window.location.hostname.startsWith('192.168.');
 export const environment = {
-    production: true,
-    // apiUrl: 'https://rashtrotthana-backend-812956739285.us-east4.run.app/api'  // Production API URL
-    //    apiUrl: 'http://192.168.9.139:3000/api', 
-    //    apiUrl:'http://34.93.41.27:3000/api'
-    // apiUrl: 'https://backend-812956739285.us-east4.run.app/api',
-    // apiUrl:'https://hrproindia.in/api'
-    // apiUrl:'http://backendapi.hrproindia.in/api'
-    // apiUrl:'http://ec2-13-60-191-28.eu-north-1.compute.amazonaws.com:3000/api',
-    // apiUrl: 'http://localhost:3000/api',
-    // apiUrl:"https://vasavi-hospitals-812956739285.us-east4.run.app/api"
-        apiUrl: isLan
-    ? 'http://192.168.9.139:3000/api'
+  production: true,
+  // apiUrl: 'https://rashtrotthana-backend-812956739285.us-east4.run.app/api'  // Production API URL
+  //    apiUrl: 'http://192.168.9.139:3000/api', 
+  //    apiUrl:'http://34.93.41.27:3000/api'
+  // apiUrl: 'https://backend-812956739285.us-east4.run.app/api',
+  // apiUrl:'https://hrproindia.in/api'
+  // apiUrl:'http://backendapi.hrproindia.in/api'
+  // apiUrl:'http://ec2-13-60-191-28.eu-north-1.compute.amazonaws.com:3000/api',
+  // apiUrl: 'http://localhost:3000/api',
+  // apiUrl:"https://vasavi-hospitals-812956739285.us-east4.run.app/api"
+  //     apiUrl: isLan
+  // ? 'http://192.168.9.139:3000/api'
+  // : '/api'
+  apiUrl: isLan
+    ? 'http://192.168.13.148:3000/api'
     : '/api'
-      // apiUrl: 'https://docmindsjmrh.imapps.in/api'
+  // apiUrl: 'https://docmindsjmrh.imapps.in/api'
 
 };
